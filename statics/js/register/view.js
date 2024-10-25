61b09cf5876661f5feafae29a3b5e0d8adf54209
krigjo25 <krigjo25@gmail.com>
2024-10-25 12:45:43 +0200

    Removed unneccesary function

--- statics/js/register/view.js
+++ statics/js/register/view.js
@@ -1,19 +1,5 @@
-function loginView()
-{
-    html = /*HTML*/`
-    <div>
-        <input type="text" onchange='placeholder="e.g j245">
-        <input type="password" placeholder='1234'>
-        <button onclick='modal.app.currentpage = 'dashboard'> login</button> <button onclick='modal.app.currentpage='register''> register</button>
-    </div>`;
-
-    document.getElementById('app').innerHTML = html;
-
-}
-
 function registerView()
 {
-
     document.getElementById('app').innerHTML = /*HTML*/ `
         <div>
         <input type="text" onchange='modal.inputs.register.uname = this.value' placeholder="e.g j245">
@@ -23,9 +9,4 @@ function registerView()
         
         <button onclick='modal.app.currentpage = 'login'> Register</button>
     </div>`;
-}
-
-function registerController() {
- 
-    
 }
