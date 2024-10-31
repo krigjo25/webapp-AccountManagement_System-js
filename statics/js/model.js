0086a899c19cf10b89124b622762093f08b0c5e4
krigjo25 <krigjo25@gmail.com>
2024-10-31 12:53:20 +0100

    Optimizing the modal

--- statics/js/model.js
+++ statics/js/model.js
@@ -5,33 +5,66 @@ let model = {
     },
     inputs: {
 
-        registration: {
+        registration: 
+        {
             name:"",
             username:"",
             email:"",
             password:"",
-            konto: [{
-                name:"",
-                type:"",
-                sum: 0,
-                id:"",
-            }],
+            brukskonto: [
+                {
+                    rent: 0.8,
+                    checked: false,
+                    name:"",
+                    type:"",
+                    sum: 0,
+                    id:"",
+                }
+            ],
         },
-        login: {
+        login: 
+        {
             uname: "",
             password:'',
         }
 
     },
-    data: {
-        users:[
+    data: 
+    {
+        users:
+        [
             {
                 id: "01016612345",
                 name: 'ole',
                 password: '1234',
                 born : '01.01-1964',
                 epost:'ole@norman.no',
-                konto:
+
+                savings:
+                [
+                    {
+                        rent: 0.8,
+                        visable: true,
+                        name:"konto1",
+                        type:'saving',
+                        sum:'10.0000.000',
+                        
+                        id: "1234.01.232",
+                        
+                    },
+                ],
+                brukskonto:
+                [
+                    {
+                        rent: 0.001,
+                        visable: true,
+                        name:"konto1",
+                        type:'brukskonto',
+                        sum:'10.0000.000',
+                        id: "1234.01.232",
+                    },
+                ],
+                savings:
                 [
                     {
                         name:"konto1",
